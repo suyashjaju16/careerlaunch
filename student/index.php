897f9cf3fa467f2cd138a1b78e0fe91c1823e0c1
Piyush <puropiyush@gmail.com>
2025-02-06 14:31:08 -0800

    info tooltip show on hover

--- student/index.php
+++ student/index.php
@@ -1312,7 +1312,10 @@ echo '<div class="row align-items-center p-0 w-100">
                         document.addEventListener("DOMContentLoaded", function () {
                             var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
                             var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-                                return new bootstrap.Popover(popoverTriggerEl);
+                                return new bootstrap.Popover(popoverTriggerEl, {
+                                    trigger: 'hover', 
+                                    html: true
+                                });
                             });
                         });
                         </script>
