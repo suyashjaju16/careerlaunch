0645d0f2c572782a0c7ba99d0d1ff7cb7504aab4
suyashjaju16 <so.jaju444@gmail.com>
2025-03-07 01:41:57 -0800

    Type Column for PrePost Added in Response Tables

--- components/responsesTable.php
+++ components/responsesTable.php
@@ -1,5 +1,6 @@
 <?php 
 $students_data = json_decode(fetch_data(API_STUDENTS_ENDPOINT,$data),true);
+$is_pre_post = $data->filters->implementation_time === "prepost" ? true : false;
 ?>
 
 <table id="datatable" class="table table-bordered dt-responsive nowrap"
@@ -10,6 +11,7 @@ $students_data = json_decode(fetch_data(API_STUDENTS_ENDPOINT,$data),true);
             <th style="color:white!important">First Name </th>
             <th style="color:white!important">Last Name</th>
             <th style="color:white!important">Email</th>
+            <?= $is_pre_post ? "<th style='color:white!important'>Type</th>" : "";?>
             <th style="color:white!important">Overall Score</th>
             <th style="color:white!important">Date</th>
             <th class="no-sort" style="width:20px;color:white!important">Report</th>
@@ -27,6 +29,7 @@ $students_data = json_decode(fetch_data(API_STUDENTS_ENDPOINT,$data),true);
             <td> <?= $name[0] ?> </td>
             <td> <?= $name[1] ?> </td>
             <td> <?= $values["Email"] ?> </td>
+            <?= $is_pre_post ? "<td>".$values["Type"]."</td>" : "";?>
             <td>
                 <center> <?= round($values["Score"],0) ?> <center>
             </td>
